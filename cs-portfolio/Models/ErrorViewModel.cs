c867847531ca3fcb2f4814fb09d91f3b3a59dd1d
Dooques <samuelgdukes@gmail.com>
2026-06-20 11:27:34 +0100

    Delete old project

--- cs-portfolio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace csPortfolio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- cs-portfolio/Models/HangulRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace csPortfolio.Models
-{
-    public class HangulRequest
-    {
-        public string text { get; set; }
-    }
-}
